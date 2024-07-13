35617c1dcbce09eedc586cdfd00e91d55f0ace9f
Pavel M <april504@yandex.ru>
2024-07-13 08:19:42 +0700

    wip: preloader upgrade

--- src/components/ui/preloader.tsx
+++ src/components/ui/preloader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { cn } from '@/lib/utils/cn';
 
+import E from '@/assets/svg/E.svg';
 interface Props {
     isVisible: boolean;
 }
@@ -10,7 +11,8 @@ const Preloader: React.FC<Props> = ({isVisible}) => {
     return (
         <div>
             <div className={cn('preloader', { isVisible })}>
-                <img className={cn('preloader__icon')}  src="/img/favicon.webp" />                
+                {/* <img className={cn('preloader__icon')}  src="/img/favicon.webp" />                 */}
+                <E className={'bob-32'} />
             </div>
         </div>
     );
