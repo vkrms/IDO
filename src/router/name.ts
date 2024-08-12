8208fb3998504b9338d1a94e6e464cf2197c35a0
Pavel M <april504@yandex.ru>
2024-08-12 11:02:13 +0700

    wip

--- src/router/name.ts
+++ src/router/name.ts
@@ -1,9 +1,9 @@
-export const LOGIN = 'https://eventflo-back.vercel.app';
+export const LOGIN = 'https://eventflo-back.vercel.app'; // get rid of probably
 
 export const HOME = '/';
-export const WHY_FLO_COIN = '/why_flo_coin';
-export const TOKENOMICS = '/tokenomics';
-export const OUR_TEAM = '/our_team';
-export const ROADMAP = '/roadmap';
+export const WHY_FLO_COIN = '/why_flo_coin'; // 404
+export const TOKENOMICS = '/tokenomics'; // 404
+export const OUR_TEAM = '/our_team'; // 404
+export const ROADMAP = '/roadmap'; // 404
 
-export const EARLY_ACCESS = 'early_access';
+export const EARLY_ACCESS = 'early_access'; // get rid of
